8416039808a7d13940d275ea0b28e7eb15009da8
CRISTHIAN MEDINA <cristhian.medina.or@hotmail.com>
2025-07-22 04:40:29 +0000

    crm_stage_default_1

---
+++ addons/crm_stage_team_required/__manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    "name": "CRM Etapas con Equipo por Defecto",
+    "version": "1.0",
+    "summary": "Asigna automáticamente el equipo al crear etapas CRM",
+    "depends": ["crm"],
+    "author": "Esteban Medina",
+    "category": "Sales",
+    "data": [
+        
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
