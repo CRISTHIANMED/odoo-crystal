1e16a5905aec13149cb15d5af14823a4d4c787f3
CRISTHIAN MEDINA <cristhian.medina.or@hotmail.com>
2025-07-30 14:55:38 +0000

    crm stage selector no listo

---
+++ addons/crm_stage_team_selector/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Selector de equipo de CRM Stage",
+    "version": "1.0",
+    "depends": ["crm", "sales_team"],
+    "author": "Cristhian Medina",
+    "category": "Sales",
+    "summary": "Asocia etapas a equipos de ventas",
+    "data": [
+        "views/crm_stage_form.xml",
+        "views/crm_lead_kanban_quick_create_disable.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
